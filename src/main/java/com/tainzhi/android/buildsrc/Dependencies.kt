73308c0b4d0dce234b73ca39a8f292cab1ad5538
tainzhi <qfq61@qq.com>
2020-03-25 20:49:57 +0800

    upgrade BaseRecyclerViewAdapterHelper to 3.0.1

--- src/main/java/com/tainzhi/android/buildsrc/Dependencies.kt
+++ src/main/java/com/tainzhi/android/buildsrc/Dependencies.kt
@@ -206,7 +206,7 @@ object Libs {
     
     const val cookietar = "com.github.franmontiel:PersistentCookieJar:v1.0.1"
     
-    const val baseRecyclerViewAdapterHelper = "com.github.CymChad:BaseRecyclerViewAdapterHelper:2.9.50"
+    const val baseRecyclerViewAdapterHelper = "com.github.CymChad:BaseRecyclerViewAdapterHelper:3.0.1"
     
     const val youthBanner = "com.youth.banner:banner:1.4.10"
     
