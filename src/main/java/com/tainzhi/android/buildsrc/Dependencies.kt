0ac0d9f66df8e4fc6ebc5a3c3333f5d65fe90f67
tainzhi <qfq61@qq.com>
2020-06-10 18:00:40 +0800

    update version

--- src/main/java/com/tainzhi/android/buildsrc/Dependencies.kt
+++ src/main/java/com/tainzhi/android/buildsrc/Dependencies.kt
@@ -187,13 +187,13 @@ object Libs {
     }
 
     object Koin {
-        private const val version = "2.1.3"
+        private const val version = "2.1.5"
         // Koin AndroidX Scope features
         const val scope = "org.koin:koin-androidx-scope:$version"
         // Koin AndroidX Architecture ViewModel
         const val viewmodel = "org.koin:koin-androidx-viewmodel:$version"
         // Koin AndroidX Fragment features
-        const val frament = "org.koin:koin-androidx-fragment:$version"
+        const val fragment = "org.koin:koin-androidx-fragment:$version"
         // Koin AndrodX Experimental features
         const val ext = "org.koin:koin-androidx-ext:$version"
         // Koin Unit test
