e3205f076285324e1912535c07052007ff5d4f5a
tainzhi <qfq61@qq.com>
2021-05-14 06:00:47 +0800

    deprecate kotlin.extension

--- src/main/java/com/tainzhi/android/buildsrc/Dependencies.kt
+++ src/main/java/com/tainzhi/android/buildsrc/Dependencies.kt
@@ -28,7 +28,6 @@ object Libs {
     object Plugins {
         const val androidApplication = "com.android.application"
         const val kotlinAndroid = "android"
-        const val kotlinExtensions = "android.extensions"
         const val kapt = "kapt"
         const val detekt = "io.gitlab.arturbosch.detekt"
         const val crashlytics = "io.fabric"
